6b12c10d1927748cfebe9998128971f390f67112
Ubuntu <ubuntu@ip-172-31-76-141.ec2.internal>
2022-03-24 02:30:42 +0000

    Aplicação dos Protocol Buffers para envio das mensagens

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import com.google.protobuf.ByteString;
 
 public class Chat {
 
@@ -17,35 +18,44 @@ public class Chat {
     Scanner sc = new Scanner(System.in);
     
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("18.215.145.181");
+    factory.setHost("54.235.231.127");
     factory.setUsername("jp");
     factory.setPassword("9910");
     factory.setVirtualHost("/");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
     
-    String QUEUE_NAME = "";
-    while(QUEUE_NAME.isEmpty())
+    String USUARIO = "";
+    while(USUARIO.isEmpty())
     {
       System.out.print("User: ");
-      QUEUE_NAME = sc.nextLine();
+      USUARIO = sc.nextLine();
     }
                       //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+    channel.queueDeclare(USUARIO, false,   false,     false,       null);
     
     Consumer consumer = new DefaultConsumer(channel) 
     {
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException 
       {
-        //O QUE O RECEPTOR FARÁ QUANDO CHEGAR UMA MENSAGEM. "body" é o conteúdo da mensagem (representado em bytes)
-
-        String message = new String(body, "UTF-8");
-        System.out.println("\n"+message);
+        // O que o receptor fará quando chegar uma mensagem. "body" é o conteúdo da mensagem (representado em bytes)
+        // Desserializar o conteúdo recebido
+        MensagemProto.Mensagem mensagem_recebida = MensagemProto.Mensagem.parseFrom(body);
+        String emissor = mensagem_recebida.getEmissor();
+        String data = mensagem_recebida.getData();
+        String hora = mensagem_recebida.getHora();
+        
+        MensagemProto.Conteudo conteudo = mensagem_recebida.getConteudo();
+        String texto = conteudo.getCorpo().toStringUtf8();
+        
+        // Formato da mensagem: (21/09/2016 às 20:53) marciocosta diz: E aí, Tarcisio! Vamos sim!
+        String msg_formatada = (  "("+data+" às "+hora+") "+emissor+" diz: "+texto   );
+        System.out.println("\n"+msg_formatada);
         System.out.print(prompt);
       }
     };
                       //(queue-name, autoAck, consumer);    
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
+    channel.basicConsume(USUARIO, true,    consumer);
     
     String destinatario = "";
     int dest_compare = -1;
@@ -76,7 +86,11 @@ public class Chat {
             }
             else //se não, enviar mensagem para o destinatário
             {
-              //formato: (21/09/2016 às 20:53) marciocosta diz: E aí, Tarcisio! Vamos sim!
+              /*
+              * Montar o protocol buffer. O arquivo "MensagemProto.java" contém todas as 'messages' (classe Mensagem e classe Conteúdo).
+              * Builders têm getters e setters. Servem para preencher o objeto. Usar .build() para transformar em uma Message.
+              * Messages têm apenas getters e são imutáveis.
+              */
               LocalDate data = java.time.LocalDate.now();
               int mes = data.getMonthValue();
               int dia = data.getDayOfMonth();
@@ -86,9 +100,23 @@ public class Chat {
               LocalTime hora = java.time.LocalTime.now();
               String hora_formatada = String.valueOf(hora.getHour()) +":"+ String.valueOf(hora.getMinute());
               
-              String conteudo = new String(msg);
-              msg = (  "("+data_formatada+" às "+hora_formatada+") "+QUEUE_NAME+" diz: "+conteudo   );
-              channel.basicPublish("",destinatario,null,msg.getBytes("UTF-8"));
+              //Montar o conteúdo da mensagem (classe Conteúdo)
+              MensagemProto.Conteudo.Builder conteudo = MensagemProto.Conteudo.newBuilder();
+              conteudo.setTipo("text/plain")
+                      .setCorpo(ByteString.copyFromUtf8(msg));
+              
+              //Montar a mensagem (classe Mensagem), setando o conteúdo para o montado anteriormente.
+              MensagemProto.Mensagem.Builder mensagem = MensagemProto.Mensagem.newBuilder();
+              MensagemProto.Mensagem mensagem_construida = 
+                mensagem.setEmissor(USUARIO)
+                        .setData(data_formatada)
+                        .setHora(hora_formatada)
+                        .setConteudo(conteudo)
+                        .build();
+                      
+              //Serializar a Mensagem em um vetor de bytes para depois enviar
+              byte[] mensagem_serializada = mensagem_construida.toByteArray();
+              channel.basicPublish("",destinatario,null,mensagem_serializada);
             }
         }
     }
